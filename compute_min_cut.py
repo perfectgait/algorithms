6afc0b6e7faef10b80b3f27994473212b662dbbc
Matt Rathbun <mrathbun80@gmail.com>
2015-02-15 22:41:46 -0800

    Remove commented code from compute minimum cuts

--- compute_min_cut.py
+++ compute_min_cut.py
@@ -50,17 +50,6 @@ def merge_edge(graph, u, v):
                 adjacency_list[j] = u
 
 
-# graph = {
-#     1: [2, 3, 4, 7],
-#     2: [1, 3, 4],
-#     3: [1, 2, 4],
-#     4: [1, 2, 3, 5],
-#     5: [4, 6, 7, 8],
-#     6: [5, 7, 8],
-#     7: [1, 5, 6, 8],
-#     8: [5, 6, 7]
-# }
-
 graph = {}
 lines = [line.strip().rstrip('\r\n').split('\t') for line in open('kargerMinCut.txt')]
 
